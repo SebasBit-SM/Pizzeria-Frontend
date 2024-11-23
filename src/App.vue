6cf476b4c642c835e2a0aac13ee4aa5a97f3dc27
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-11-22 23:50:04 -0500

    Se ajusta vista

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/pedidos">Gestion de Pedidos</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/pedidos">Gestion de Pedidos</router-link> |
   </nav>
   <router-view/>
 </template>
