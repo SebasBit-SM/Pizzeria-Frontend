fe6efbda04074f7016a40a32e2e1eb21da4bb9b3
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-11-22 23:41:14 -0500

    Se agrega template a la vista de pedidos

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+      <h1 class="my-4">Gestión de Pedidos</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Cliente</th>
+            <th>Estado</th>
+            <th>Total</th>
+            <th>Tipo de Entrega</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pedido in pedidos" :key="pedido.id">
+            <td>{{ pedido.id }}</td>
+            <td>{{ pedido.cliente }}</td>
+            <td>{{ pedido.estado }}</td>
+            <td>{{ pedido.total | formatoMoneda }}</td>
+            <td>{{ pedido.tipoEntrega }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
