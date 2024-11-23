f62f984220eee2ccbaba2b8a6791227c3e0283e0
miguelserr <aristizabalmiguelangel50@gmail.com>
2024-11-23 01:08:28 -0500

    Se implementan las rutas al index

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,39 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
+// Importar los componentes
+import Home from '../views/Home.vue';
+import Pedidos from '../views/Pedidos.vue';
+import Inventario from '../views/Inventario.vue';
+import Sucursales from '../views/Sucursales.vue';
+
+// Definir las rutas
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/pedidos',
+    name: 'Pedidos',
+    component: Pedidos,
+  },
+  {
+    path: '/inventario',
+    name: 'Inventario',
+    component: Inventario,
+  },
+  {
+    path: '/sucursales',
+    name: 'Sucursales',
+    component: Sucursales,
+  },
+];
 
+// Crear el router
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
