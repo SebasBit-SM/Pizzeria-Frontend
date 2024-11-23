259a8a9735cca6f254a70840a3f4d8ef97524951
Sebastian Sañudo <sebas2004lupe@gmail.com>
2024-11-22 23:47:47 -0500

    Se crea acceso en el app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/pedidos">Gestion de Pedidos</router-link>
   </nav>
   <router-view/>
 </template>
