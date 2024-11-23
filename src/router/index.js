046fbdd3fe15e6e47d8410fc36acb446b9d8731e
miguelserr <aristizabalmiguelangel50@gmail.com>
2024-11-23 14:36:26 -0500

    Se corrige rutas en el index  y se crea vista usuarios

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-<<<<<<< HEAD
-// Importar los componentes
 import Home from '../views/Home.vue';
+import Usuarios from '../views/Usuarios.vue';
 import Pedidos from '../views/Pedidos.vue';
 import Inventario from '../views/Inventario.vue';
 import Sucursales from '../views/Sucursales.vue';
+import Proveedores from '../views/Proveedores.vue';
 
-// Definir las rutas
-=======
-import Pedidos from '../views/Pedidos.vue';
-
->>>>>>> d7c6b34255f956f03c56d358492eb58da03a07b2
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/pedidos',
-    name: 'Pedidos',
-<<<<<<< HEAD
-    component: Pedidos,
-  },
-  {
-    path: '/inventario',
-    name: 'Inventario',
-    component: Inventario,
-  },
-  {
-    path: '/sucursales',
-    name: 'Sucursales',
-    component: Sucursales,
-  },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/usuarios', name: 'Usuarios', component: Usuarios },
+  { path: '/pedidos', name: 'Pedidos', component: Pedidos },
+  { path: '/inventario', name: 'Inventario', component: Inventario },
+  { path: '/sucursales', name: 'Sucursales', component: Sucursales },
+  { path: '/proveedores', name: 'Proveedores', component: Proveedores },
 ];
-=======
-    component: Pedidos
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
->>>>>>> d7c6b34255f956f03c56d358492eb58da03a07b2
 
-// Crear el router
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
