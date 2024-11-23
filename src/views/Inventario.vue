5b7d90acd02e716b50c0dfaecb89c94f9181444a
miguelserr <aristizabalmiguelangel50@gmail.com>
2024-11-23 01:01:44 -0500

    Se complementa inventario

---
+++ src/views/Inventario.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+      <h1 class="my-4">Gestión de Inventario</h1>
+      <table class="table table-bordered">
+        <thead class="table-dark">
+          <tr>
+            <th>#</th>
+            <th>Material</th>
+            <th>Unidad</th>
+            <th>Stock Actual</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="material in materiales" :key="material.id">
+            <td>{{ material.id }}</td>
+            <td>{{ material.nombre }}</td>
+            <td>{{ material.unidad }}</td>
+            <td>{{ material.stock_actual }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
